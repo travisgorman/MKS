4d3597e873e35d2bfe995f050c1a932f00330b22
Travis Gorman <thefarbetween@me.com>
2016-01-08 10:31:55 -0600

    Update mks-admissions-challenge-1.js

--- AdmissionsChallenge1/mks-admissions-challenge-1.js
+++ AdmissionsChallenge1/mks-admissions-challenge-1.js
@@ -3,3 +3,14 @@ function decryptA(message){
     .map( word => word.slice(-1))
       .join('');
 }
+
+function decryptB(message){ 
+  var password = "",
+  words = message.split(' ');
+  words.forEach( word => {
+    var first = word.charAt(0),
+      last = word.charAt( word.length -1);
+        password += (( first > last ) ? first : last )
+  });
+  return password;  
+} 
